7adf20bbf1b742ee00c8e4356795ba0e1acde20a
Alkis <a.gkreen@gmail.com>
2021-01-04 14:57:54 +0100

    fundamentals-5

--- Fundamentals 5/caesar/caesar.js
+++ Fundamentals 5/caesar/caesar.js
@@ -1,5 +1,39 @@
-const caesar = function() {
+let mode = "ceaser";
+let shift;
 
+function encrypt(text, shift) {
+    let result = "";
+    if (mode == "ceaser") {
+
+
+        for (let i = 0; i < text.length; i++) {
+
+
+            let c = text.charCodeAt(i);
+
+
+            if (c >= 65 && c <= 90) {
+                result += String.fromCharCode((c - 65 + shift) % 26 + 65);
+
+
+            } else if (c >= 97 && c <= 122) {
+                result += String.fromCharCode((c - 97 + shift) % 26 + 97);
+
+
+            } else {
+                result += text.charAt(i);
+            }
+        }
+    }
+    return result;
 }
+
+function decrypt(text, shift) {
+    let result = "";
+    shift = (26 - shift) % 26;
+    result = encrypt(text, shift);
+    return result;
+}
+
 
 module.exports = caesar
--- Fundamentals 5/fibonacci/fibonacci.js
+++ Fundamentals 5/fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (n < 2) {
+        return n
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 module.exports = fibonacci
